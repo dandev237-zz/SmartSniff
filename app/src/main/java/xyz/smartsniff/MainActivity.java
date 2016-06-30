2e8427c17fbe5cecfaea3084947b5e322838face
Daniel Castro García <dandev237@gmail.com>
2016-06-30 03:11:45 +0200

    Added SessionDatabaseHelper
    + SessionDatabaseHelper.java
    + Minor UI modifications
    + String resources added

--- app/src/main/java/xyz/smartsniff/MainActivity.java
+++ app/src/main/java/xyz/smartsniff/MainActivity.java
@@ -3,8 +3,13 @@ package xyz.smartsniff;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.TableLayout;
+import android.widget.ToggleButton;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -12,10 +17,21 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+/**
+ * Main activity of the application. Contains the scanning interface and the
+ * appbar.
+ *
+ * Autor: Daniel Castro García
+ * Email: dandev237@gmail.com
+ * Fecha: 29/06/2016
+ */
+
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
 
     private Toolbar appBar;
     private MapFragment mapFragment;
+    private TableLayout scanLayout;
+    private ToggleButton scanButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +43,23 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        scanLayout = (TableLayout) findViewById(R.id.scanLayout);
+        scanLayout.setVisibility(View.INVISIBLE);
+
+        scanButton = (ToggleButton) findViewById(R.id.scanToggleButton);
+        scanButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if(isChecked){
+                    Log.d("Scan Button", "SCAN BUTTON PRESSED. STATUS: CHECKED");
+                    //TODO: Begin scanning procedure
+                }else{
+                    Log.d("Scan Button", "SCAN BUTTON PRESSED. STATUS: UNCHECKED");
+                    //TODO: Stop scanning procedure
+                }
+            }
+        });
     }
 
     //Action bar
@@ -46,7 +79,18 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
+        if (id == R.id.action_delete_data) {
+            Log.d("AppBar Delete", "APPBAR: DELETE BUTTON PRESSED");
+            return true;
+        }
+
+        if (id == R.id.action_send_data) {
+            Log.d("AppBar Send", "APPBAR: SEND BUTTON PRESSED");
+            return true;
+        }
+
         if (id == R.id.action_settings) {
+            Log.d("AppBar Configuration", "APPBAR: CONFIGURATION BUTTON PRESSED");
             return true;
         }
 
---
+++ app/src/main/java/xyz/smartsniff/SessionDatabaseHelper.java
@@ -0,0 +1,136 @@
+package xyz.smartsniff;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * This class handles database operations such as reading, writing and upgrading.
+ *
+ * Autor: Daniel Castro García
+ * Email: dandev237@gmail.com
+ * Fecha: 30/06/2016
+ */
+public class SessionDatabaseHelper extends SQLiteOpenHelper {
+    //Singleton instance
+    private static SessionDatabaseHelper singletonInstance;
+
+    //Database info
+    private static final String DATABASE_NAME = "sessionsDatabase";
+    private static final int DATABASE_VERSION = 1;
+
+    //Table Names
+    private static final String TABLE_SESSIONS = "sessions";
+    private static final String TABLE_DEVICES = "devices";
+    private static final String TABLE_LOCATIONS = "locations";
+    private static final String TABLE_ASOCSESSIONSDEVICES = "asocSessionsDevices";
+
+    //Session Table Columns
+    private static final String KEY_SESSION_ID = "id";
+    private static final String KEY_SESSION_STARTDATE = "startDate";
+    private static final String KEY_SESSION_ENDDATE = "endDate";
+
+    //Device Table Columns
+    private static final String KEY_DEVICE_ID = "id";
+    private static final String KEY_DEVICE_SSID = "ssid";
+    private static final String KEY_DEVICE_BSSID = "bssid";
+    private static final String KEY_DEVICE_CHARACTERISTICS = "characteristics";
+    private static final String KEY_DEVICE_TYPE = "type";
+
+    //Location Table Columns
+    private static final String KEY_LOCATION_ID = "id";
+    private static final String KEY_LOCATION_DATE = "date";
+    private static final String KEY_LOCATION_COORDINATES = "coordinates";
+
+    //Association Table Columns
+    private static final String KEY_ASSOCIATION_ID_SESSION_FK = "idSession";
+    private static final String KEY_ASSOCIATION_ID_DEVICE_FK = "idDevice";
+    private static final String KEY_ASSOCIATION_ID_LOCATION_FK = "idLocation";
+
+    /*
+    * Singleton pattern
+    * In order to access the database connection:
+    * SessionDatabaseHelper dbHelper = SessionDatabaseHelper.getInstance(this);
+    */
+    public static synchronized SessionDatabaseHelper getInstance(Context context){
+        if (singletonInstance == null){
+            singletonInstance = new SessionDatabaseHelper(context.getApplicationContext());
+        }
+        return singletonInstance;
+    }
+
+    //Constructor is private to prevent direct instantiation
+    private SessionDatabaseHelper(Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    /*
+     * Configures database settings. Called when the database connection
+     * is being configured.
+     */
+    @Override
+    public void onConfigure(SQLiteDatabase db){
+        super.onConfigure(db);
+        db.setForeignKeyConstraintsEnabled(true);
+    }
+
+    /*
+     * Creates the database for the first time. If it already exists, this
+     * method will not be called.
+     */
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String CREATE_SESSIONS_TABLE = "CREATE TABLE " + TABLE_SESSIONS +
+                "(" +
+                KEY_SESSION_ID + " INTEGER PRIMARY KEY," +
+                KEY_SESSION_STARTDATE + " TEXT," +
+                KEY_SESSION_ENDDATE + " TEXT" +
+                ")";
+
+        String CREATE_DEVICES_TABLE = "CREATE TABLE " + TABLE_DEVICES +
+                "(" +
+                KEY_DEVICE_ID + " INTEGER PRIMARY KEY," +
+                KEY_DEVICE_SSID + " TEXT," +
+                KEY_DEVICE_BSSID + " TEXT," +
+                KEY_DEVICE_CHARACTERISTICS + " TEXT," +
+                KEY_DEVICE_TYPE + " TEXT" +
+                ")";
+
+        String CREATE_LOCATIONS_TABLE = "CREATE TABLE " + TABLE_LOCATIONS +
+                "(" +
+                KEY_LOCATION_ID + " INTEGER PRIMARY KEY," +
+                KEY_LOCATION_DATE + " TEXT," +
+                KEY_LOCATION_COORDINATES + " TEXT" +
+                ")";
+
+        String CREATE_ASSOCIATION_TABLE = "CREATE TABLE " + TABLE_ASOCSESSIONSDEVICES +
+                "(" +
+                KEY_ASSOCIATION_ID_SESSION_FK + " INTEGER REFERENCES " + TABLE_SESSIONS + "," +
+                KEY_ASSOCIATION_ID_DEVICE_FK + " INTEGER REFERENCES " + TABLE_DEVICES + "," +
+                KEY_ASSOCIATION_ID_LOCATION_FK + " INTEGER REFERENCES " + TABLE_LOCATIONS +
+                ")";
+
+        sqLiteDatabase.execSQL(CREATE_SESSIONS_TABLE);
+        sqLiteDatabase.execSQL(CREATE_DEVICES_TABLE);
+        sqLiteDatabase.execSQL(CREATE_LOCATIONS_TABLE);
+        sqLiteDatabase.execSQL(CREATE_ASSOCIATION_TABLE);
+    }
+
+    /*
+     * Called when the database needs to be upgraded (the database already
+     * exists and the version is different from the version of the database that
+     * exists in memory).
+     */
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        //Simplest implementation possible
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_SESSIONS);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_DEVICES);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_LOCATIONS);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_ASOCSESSIONSDEVICES);
+
+        onCreate(sqLiteDatabase);
+    }
+
+    //TODO: ADD CRUD OPERATIONS
+}
