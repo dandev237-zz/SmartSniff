f550b80f295f824ebf3495682b0ef320f099c956
Daniel Castro García <dandev237@gmail.com>
2016-07-03 19:44:50 +0200

    First implementation of CustomBroadcastReceiver.java
    +CustomBroadcastReceiver.java

---
+++ app/src/main/java/xyz/smartsniff/CustomBroadcastReceiver.java
@@ -0,0 +1,152 @@
+package xyz.smartsniff;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.WifiManager;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import java.util.List;
+
+/**
+ * Custom BroadcastReceiver, designed to perform continuous network scanning.
+ *
+ * Autor: Daniel Castro García
+ * Email: dandev237@gmail.com
+ * Fecha: 03/07/2016
+ */
+public class CustomBroadcastReceiver extends BroadcastReceiver{
+
+    private Handler scanHandler = new Handler();
+
+    @NonNull
+    private Context context;
+    private WifiManager wifiManager;
+    @NonNull
+    private ScanResultsListener scanResultsListener;
+    private boolean continueScanning = false;
+
+    private long lastScanTime = 0;
+    private int scanInterval; //in milliseconds
+    private Runnable scanRunnable = new Runnable() {
+        @Override
+        public void run() {
+            initiateScan();
+        }
+    };
+
+    /**
+     * Constructor used to specify the delivery rate of the ScanResults.
+     *
+     * @param context   cannot be null
+     * @param listener  cannot be null
+     * @param interval  preferred delay between scans, in milliseconds. Cannot be negative
+     *
+     * @throws NullPointerException if {@code context} or {@code listener} is null
+     * @throws IllegalArgumentException if {@code interval} is negative
+     */
+    public CustomBroadcastReceiver (@NonNull Context context, @NonNull ScanResultsListener listener, int interval){
+        checkContext(context);
+        checkListener(listener);
+        checkInterval(interval);
+
+        this.context = context.getApplicationContext();
+        scanResultsListener = listener;
+        scanInterval = interval;
+        wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+    }
+
+    private void checkInterval(int interval) {
+        if(interval < 0)
+            throw new IllegalArgumentException("interval cannot be negative");
+    }
+
+    private void checkListener(@NonNull ScanResultsListener listener) {
+        if(listener == null)
+            throw new NullPointerException("listener cannot be null");
+    }
+
+    private void checkContext(@NonNull Context context) {
+        if(context == null)
+            throw new NullPointerException("context cannot be null");
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        //Check if this receiver was registered for the appropriate action
+        if(!WifiManager.SCAN_RESULTS_AVAILABLE_ACTION.equals(intent.getAction()))
+            throw new IllegalStateException("WRONG ACTION: CustomBroadcastReceiver registered for " + intent.getAction());
+
+
+        if(continueScanning){
+            initiateScan();
+            scanResultsListener.onScanResultsReceived(wifiManager.getScanResults());
+        }
+    }
+
+    private void initiateScan(){
+        long scanTime = System.currentTimeMillis();
+
+        if(continueScanning){
+            long delay = scanTime - lastScanTime;
+            if(scanInterval == 0 || delay >= scanInterval){
+                //Send the results now
+                wifiManager.startScan();
+                lastScanTime = scanTime;
+            } else {
+                //Send the results when the appropriate time comes
+                scanHandler.removeCallbacks(scanRunnable);
+                scanHandler.postDelayed(scanRunnable, scanInterval - delay);
+            }
+        }
+    }
+
+    /**
+     * Method to change the rate (in milliseconds) at which the app receives the ScanResults
+     *
+     * @param interval  preferred delay between scans, in milliseconds. Cannot be negative
+     * @throws IllegalArgumentException     if interval is negative
+     */
+    public void changeScanInterval(int interval){
+        checkInterval(interval);
+        scanInterval = interval;
+    }
+
+    /**
+     * Method to initiate a WiFi scan.
+     * @param releaseResultsImmediately whether or not to release ScanResults immediately from the last scan
+     */
+    public void startScan(boolean releaseResultsImmediately){
+        if(continueScanning){
+            Log.d("CustomBroadcastReceiver", "A SCAN IS ALREADY IN PROGRESS");
+            return;
+        }
+        context.registerReceiver(this, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+        continueScanning = true;
+        wifiManager.startScan();
+        lastScanTime = System.currentTimeMillis();
+        if(releaseResultsImmediately)
+            scanResultsListener.onScanResultsReceived(wifiManager.getScanResults());
+    }
+
+    /**
+     * Method to stop an initiated scan.
+     */
+    public void stopScan(){
+        if(continueScanning){
+            scanHandler.removeCallbacks(scanRunnable);
+            continueScanning = false;
+            context.unregisterReceiver(this);
+        }
+    }
+
+    /**
+     * ScanResults delivery
+     */
+    public interface ScanResultsListener{
+        void onScanResultsReceived(List results);
+    }
+}
